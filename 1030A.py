1097e2546eabdc862181bec390411979d0f62532
Muzahid <muzahidsife@gmail.com>
2024-04-06 09:22:55 +0600

    A. In Search of an Easy Problem

---
+++ 1030A.py
@@ -0,0 +1,18 @@
+a = int(input())
+
+v = input().split()
+
+b = []
+
+for i in range(a):
+    b.append(int(v[i]))
+
+d = 1
+
+for i in (b):
+    if d in (b):
+        print("HARD")
+        break
+    else:
+        print("EASY")
+        break
