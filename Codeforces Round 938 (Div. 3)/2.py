31a64573dc9691744b99eff607c2a3750f178298
Muzahid <muzahidsife@gmail.com>
2024-04-08 21:14:22 +0600

    problem 2 solved

--- Codeforces Round 938 (Div. 3)/2.py
+++ Codeforces Round 938 (Div. 3)/2.py
@@ -1,15 +1,31 @@
-def is_progressive_square(n, c, d, matrix):
-    a_1_1 = matrix[0][0]
-    for i in range(n):
-        for j in range(n):
-            expected_value = a_1_1 + i * c + j * d
-            if matrix[i][j] != expected_value:
-                return "NO"
-    return "YES"
+def get_input():
 
-t = int(input())
-for _ in range(t):
-    n, c, d = map(int, input().split())
-    elements = list(map(int, input().split()))
-    matrix = [elements[i:i+n] for i in range(0, len(elements), n)]
-    print(is_progressive_square(n, c, d, matrix))
+    t = int(input())
+    test_cases = []
+
+    for _ in range(t):
+
+        n, c, d = map(int, input().split())
+        
+
+        elements = list(map(int, input().split()))
+        
+        test_cases.append((n, c, d, elements))
+    
+    return test_cases
+def is_progressive_square(n, c, d, elements):
+
+    a1_1 = min(elements)
+    
+
+    square = [[a1_1 + i * c + j * d for j in range(n)] for i in range(n)]
+    
+
+    flat_square = [item for sublist in square for item in sublist]
+    
+
+    return sorted(elements) == sorted(flat_square)
+
+test_cases = get_input()
+for n, c, d, elements in test_cases:
+    print("YES" if is_progressive_square(n, c, d, elements) else "NO")
