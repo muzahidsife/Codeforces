3a6640993bec320e68d18df3e08a3a847a600848
Muzahid <muzahidsife@gmail.com>
2024-04-08 22:51:51 +0600

    failed time limit

--- Codeforces Round 938 (Div. 3)/3.py
+++ Codeforces Round 938 (Div. 3)/3.py
@@ -2,30 +2,54 @@ def kraken_attack(n, k, durability):
     sunk_ships = 0
     first_ship = 0
     last_ship = n - 1
+    while first_ship <= last_ship and durability[first_ship] == 0:
+        first_ship += 1
+        sunk_ships += 1
+    while last_ship >= first_ship and durability[last_ship] == 0:
+        last_ship -= 1
+        sunk_ships += 1
+
     for attack in range(k):
-        if first_ship > last_ship:  
+        if first_ship > last_ship:
             break
-        if attack % 2 == 0:  
+        if attack % 2 == 0:
             if durability[first_ship] > 0:
                 durability[first_ship] -= 1
                 if durability[first_ship] == 0:
                     sunk_ships += 1
                     first_ship += 1
-        else: 
+                    while first_ship <= last_ship and durability[first_ship] == 0:
+                        first_ship += 1
+                        sunk_ships += 1
+        else:
             if durability[last_ship] > 0:
                 durability[last_ship] -= 1
                 if durability[last_ship] == 0:
                     sunk_ships += 1
                     last_ship -= 1
+                    while last_ship >= first_ship and durability[last_ship] == 0:
+                        last_ship -= 1
+                        sunk_ships += 1
 
     return sunk_ships
 
-def process_input_output():
-    t = int(input().strip())
+def simulate_input_output(test_cases):
     results = []
-    for _ in range(t):
-        n, k = map(int, input().strip().split())
-        durability = list(map(int, input().strip().split()))
+    for n, k, durability in test_cases:
         results.append(kraken_attack(n, k, durability))
-    print('\n'.join(map(str, results)))
-process_input_output()
+    return results
+
+def main():
+    num_cases = int(input())
+    test_cases = []
+    for _ in range(num_cases):
+        n, k = map(int, input().split())
+        durability = list(map(int, input().split()))
+        test_cases.append((n, k, durability))
+
+    results = simulate_input_output(test_cases)
+    for result in results:
+        print(result)
+
+if __name__ == "__main__":
+    main()
