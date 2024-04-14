fda49c4b8922001f0bae2233f98521831f463069
Muzahid <muzahidsife@gmail.com>
2024-04-14 11:01:45 +0600

    solved

--- 110A.py
+++ 110A.py
@@ -1,12 +1,1 @@
-n = list(map(int, input().split()))
-
-count = 0
-
-for num in n:
-    if num == 4 or num == 7:
-        count += 1
-
-if count == 4 or count == 7:
-    print("YES")
-else:
-    print("NO")
+print('NYOE S'[sum(i in'47'for i in input())in(4,7)::2])
