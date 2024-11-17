b15989117506b84c4317761ee8bcca5579f79df5
Muzahid <muzahidsife@gmail.com>
2024-11-17 12:01:33 +0600

    Solved after getting wrong answer on test case 26

---
+++ 122A.py
@@ -0,0 +1,19 @@
+n = int(input())
+
+listt = [4,7,47,74,444,447,474,477,744,747,774,777]
+
+m = 0
+
+for i in listt:
+    if n%i == 0:
+        m += 1
+        continue
+
+if m > 0:
+    print("YES")
+else:
+    print("NO")
+
+
+
+
